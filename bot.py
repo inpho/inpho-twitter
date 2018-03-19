2e2c37378ad37418542e84c310e2fbff53d5cd09
colihanvm <vanessa.colihan@gmail.com>
2018-03-19 19:19:02 -0400

    streaming and responding functionality added

--- bot.py
+++ bot.py
@@ -1,18 +1,32 @@
 import tweepy, time
 from keys import *
 
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status)
+        time.sleep(60) #change later to be variable 60-600
+        api.update_status('@nesscoli test response', status.id)
+        print(status.text)
+        
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-#if specific user tweets, get tweet id, respond to tweet
 
-#userID of dummy test account
-userID = 975141243348049921
+userID = 975141243348049921 #userID of dummy test account
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
+streamListener = MyStreamListener()
+stream = tweepy.Stream(auth, streamListener)
+stream.filter(follow=[str(userID)]) #returns current tweets from specified user
 
-    if tweet.user.id == userID:
-        api.update_status('@nesscoli test response', tweet.id)
-        print('tweet success!')
+#old test code: this searched through timeline for user's
+#tweet to respond to. Effective but not right solution
+
+##public_tweets = api.home_timeline()
+##for tweet in public_tweets:
+##
+##    if tweet.user.id == userID:
+##        #should check that we haven't already responded
+##        time.sleep(60) #change later to be variable 60-600
+###        api.update_status('@nesscoli test response', tweet.id)
+##        print(tweet)
