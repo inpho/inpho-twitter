b4755542ca5de49c61edb7d39b327e2b7c12e32e
colihanvm <vanessa.colihan@gmail.com>
2018-04-01 22:22:04 -0400

    test script update

--- timelinetest.py
+++ timelinetest.py
@@ -1,4 +1,5 @@
-import tweepy, urllib, json, webbrowser
+# -*- coding: utf-8 -*-
+import tweepy, urllib, json, webbrowser, sys
 from keys import *
 #from bot import buildURL, lookUp, createResponse #not working, need to fix
 
@@ -28,21 +29,21 @@ def lookUp (url):
         if isMultiple(inpho_json):
             f.write(', after removing an article(s)')
             return True;
-        else: #no results at all
-            print('no result was found')
     else: #1 result found
-        print('a result was found')
         f.write('found after removing an article(s)')
         response = createResponse(inpho_json['url'], title)
         return True;
     return False;
 
+#function used to check if the query returns more than one result
+#returns true if more than one result is found
+    #note that the first result is chosen for the response
+#reutrns false otherwise
 def isMultiple (inpho_json):
     resDat = inpho_json.get('responseData')
     res = resDat.get('results')
     if len(res) > 0: #there was >1 result. choose 1st result
         url = res[0].get('url')
-        print('found >1 result and chose 1st option')
         f.write('found >1 result and chose 1st option')
         response = createResponse(url, title)
         return True;
@@ -54,7 +55,6 @@ def createResponse (url, title):
     link = 'https://www.inphoproject.org' + url
 #    webbrowser.open(link, new=2) #opens link in new tab of default browser
     response = 'InPhO - ' + title + ' - ' + link
-    print(response)
     return response;
 
 
@@ -63,44 +63,46 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 userID = 12450802 #peoppenheimer's twitter ID
-timeline = api.user_timeline(user_id = userID, count = 10)
-
+timeline = api.user_timeline(user_id = userID, count = 100)
+i = 0
 f = open('results.txt', 'w')
 for status in timeline:
     broken_tweet = status.text.split(" ")
     if broken_tweet[0] != 'SEP:': #tweet wasn't a SEP tweet
         print('not SEP tweet')
-        exit()
+        continue;
     del broken_tweet[0]
     del broken_tweet[len(broken_tweet)-1]
     del broken_tweet[len(broken_tweet)-1]
-
-    url, title = buildURL(broken_tweet);
-    f.write('\n' + title + ': ')
-    print('\nsearching for ' + title)
-    url = 'https://www.inphoproject.org/entity.json?redirect=true&q=' + url
-    inpho_json = json.load(urllib.urlopen(url))
-    
-    if 'url' not in inpho_json: #flag for missing page
-        print('page missing! couldn\'t find ' + title) #in future: post to text file? send alert?
-        if not isMultiple(inpho_json): 
-            found = False
-            #now, remove one "extra" word at a time (articles, conjunctions)
-            for word in broken_tweet:
-                if word.lower() == 'the' or word.lower() == 'of' or word.lower() == 'a' or \
-                    word.lower() == 'an' or word.lower() == 'for' or word.lower() == 'and' or \
-                    word.lower() == 'but' or word.lower() == 'or' or word.lower() == 'yet':
-                    broken_tweet.remove(word)
-                    url, temp = buildURL(broken_tweet)
-                    print('searching instead for ' + temp)
-                    found = lookUp(url) #try to find page each time a word is removed
-                    if found:
-                        break;
-            if not found:
-                print('attemping to remove articles did not help find ' + title + '. url was: ' + url)
-                f.write('could not find!')
+    i = i + 1
+    print(i)
+    url, title = buildURL(broken_tweet)
+    if i == 16 or i == 62 or i == 73 or i == 78:
+        f.write('\n' + 'Santaraksita*')
+        print('skipped ' + str(i))
     else:
-        f.write('found on first try')
-        response = createResponse(inpho_json['url'], title)
+
+        f.write('\n' + title + ': ')
+        url = 'https://www.inphoproject.org/entity.json?redirect=true&q=' + url
+        inpho_json = json.load(urllib.urlopen(url))
+        
+        if 'url' not in inpho_json: #flag for missing page
+            if not isMultiple(inpho_json): 
+                found = False
+                #now, remove one "extra" word at a time (articles, conjunctions)
+                for word in broken_tweet:
+                    if word.lower() == 'the' or word.lower() == 'of' or word.lower() == 'a' or \
+                        word.lower() == 'an' or word.lower() == 'for' or word.lower() == 'and' or \
+                        word.lower() == 'but' or word.lower() == 'or' or word.lower() == 'yet':
+                        broken_tweet.remove(word)
+                        url, temp = buildURL(broken_tweet)
+                        found = lookUp(url) #try to find page each time a word is removed
+                        if found:
+                            break;
+                if not found:
+                    f.write('!!!!!!!!!!!!!!!!!!!!!!!could not find!!!!!!!!!!!!!!!!!!!!!!!')
+        else:
+            f.write('found on first try')
+            response = createResponse(inpho_json['url'], title)
 
 f.close()
