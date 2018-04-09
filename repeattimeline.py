bbc8f50b425ec9c94f7be233d5da721c02a3d9bd
colihanvm <vanessa.colihan@gmail.com>
2018-04-09 15:01:09 -0400

    test script for mirroring peoppenheimer timeline

---
+++ repeattimeline.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import tweepy, urllib, json, webbrowser
+from testkeys import *
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+userID = 12450802 #peoppenheimer's twitter ID
+timeline = api.user_timeline(user_id = userID, count = 10)
+
+for status in timeline:
+    status.text
+    api.update_status(status.text)
