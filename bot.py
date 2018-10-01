3b7c7420f2a10cb9c247376e75888a177aab5444
colihanvm <vanessa.colihan@gmail.com>
2018-10-01 16:33:33 -0400

    functional quickreply tweet. notes: needs tweepy patch

--- bot.py
+++ bot.py
@@ -80,6 +80,10 @@ def createResponse (url, title):
     response = ''
     for entry in soup.find_all('item'):
         if str(entry.title) == '<title>' + title + '</title>':
+
+            author = (str(entry.find('dc:creator'))[12:-13])
+            tryDM(author)
+            
             start = str(entry.description).find('[')
             end = str(entry.description).find(']')
             if start == -1 or end == -1:
@@ -99,6 +103,7 @@ def createResponse (url, title):
         emoji = u'\U0001F5C2' #files emoji
     else:
         emoji = u'\U0001F4AD' #default thought bubble emoji
+    emoji = 'em'
     response = 'SEP\'s \"' + title + '\" is a ' + response + '\n\nCheck it out on InPhO ' + emoji + ' ' + link
     return response;
 
@@ -141,6 +146,57 @@ def sendEmail(title, err):
     except:
         print('error sending mail')
 
+def tryDM(author):
+    andIndex = author.find(' and ')
+    if andIndex > -1:
+        if ',' in author: #3 or more authors
+            authors = author.split(', ') #remove commas
+            authors[-1] = authors[-1][4:] #remove 'and ' from last entry
+        else: #2 authors
+            authors = [author[:andIndex], author[andIndex+5:]]
+    else: #1 author
+        authors = author
+    for name in authors: #create a DM for each author
+        searchResult = api.search_users(name)
+        if(searchResult):
+            createDM(name, searchResult)
+
+def createDM(name, result):
+    msg = 'May have found a match for ' + name + '. Do any of these sound right?'
+    options = [{"label": "None are good", "description": "Send none", "metadata": "none"}]
+    for user in result: #delineate each 'option' to choose
+        label = user.screen_name
+        description =  ""
+        if(user.location):
+            description = description + "loc:" + user.location
+        if(user.description and len(user.description)+5 < len(description)): #limit of 72 chars
+            description = description + " bio:" + user.description
+        if(not description):
+            description = "No info found"
+        metadata = label
+        options.append({"label": label, "description": description, "metadata": metadata})
+        
+    event = {
+      "event": {
+        "type": "message_create",
+        "message_create": {
+          "target": {
+            "recipient_id": 975141243348049921
+          },
+          "message_data": {
+            "text": msg,
+            "quick_reply": {
+              "type": "options",
+              "options": options
+            }
+          }
+        }
+      }
+    }
+    api.send_direct_message_new(event)
+        
+        
+
 #authentication for twitter bot access
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
@@ -156,12 +212,12 @@ server.login(gmail_sender, gmail_passwd)
 userID = 12450802 #userID of peoppenheimer
 myID = 974652683788455936
 myCount = api.get_user(myID).statuses_count
-last_reply_id = getLastReply(myID, myCount, userID)
+last_reply_id = 0#getLastReply(myID, myCount, userID) #########################
 
 if last_reply_id == -1:
     sendEmail('Initializing error: ', 'cannot find last peoppenheimer reply')
 else:
-    timeline = api.user_timeline(user_id = userID, count = 5) #change 5 based on frequency of running bot
+    timeline = api.user_timeline(user_id = userID, count = 1)#5) #change 5 based on frequency of running bot #######################
     #note count must be <=15 to be able to read info from rss feed (only shows 15 latest)
     last_index = len(timeline)
     for x in range(0, len(timeline)):    
@@ -201,5 +257,6 @@ else:
                     if validUrl(inpho_json['url']):
                         response = createResponse(inpho_json['url'], title)
                         if response != '':
-                            time.sleep(random.randint(60, 120))
-                            api.update_status('@peoppenheimer ' + response + ' https://twitter.com/peoppenheimer/status/' + status.id_str, status.id)
+#                            time.sleep(random.randint(60, 120))
+#                            api.update_status('@peoppenheimer ' + response + ' https://twitter.com/peoppenheimer/status/' + status.id_str, status.id)
+                            print(response)
